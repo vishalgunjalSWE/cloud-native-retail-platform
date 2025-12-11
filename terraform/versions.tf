9c15fb61e9e9323d349d99ffe33e0504be894b6e
Vishal Gunjal <vishalgunjal0287@gmail.com>
2025-12-11 11:20:19 +0530

    Add versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,38 @@
+# TERRAFORM AND PROVIDER VERSIONS
+
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+
+# PROVIDER CONFIGURATIONS
+
+provider "aws" {
+  region = var.aws_region
+}
